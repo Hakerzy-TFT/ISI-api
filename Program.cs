6475ae6519fd94e9e0e8b0a1e8deb3ef3d92fdd8
ReformedMrozu <p.mroz.zywiol@gmail.com>
2021-05-30 16:23:49 +0200

    hotfix typo

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
