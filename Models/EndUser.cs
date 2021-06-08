dd06063f5701e0f27042e2a718fca4036fb92542
Kacper <kacper.plusaa@gmail.com>
2021-06-08 18:24:41 +0200

    fixed EF via Scaffold

--- Models/EndUser.cs
+++ Models/EndUser.cs
@@ -9,6 +9,7 @@ namespace gamespace_api.Models
     {
         public EndUser()
         {
+            Bugs = new HashSet<Bug>();
             EndUserSecurities = new HashSet<EndUserSecurity>();
             GameUsers = new HashSet<GameUser>();
             Reviews = new HashSet<Review>();
@@ -16,13 +17,17 @@ namespace gamespace_api.Models
         }
 
         public int Id { get; set; }
+        public string Username { get; set; }
         public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public int Wallet { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public int UserTypeId { get; set; }
 
         public virtual UserType UserType { get; set; }
+        public virtual ICollection<Bug> Bugs { get; set; }
         public virtual ICollection<EndUserSecurity> EndUserSecurities { get; set; }
         public virtual ICollection<GameUser> GameUsers { get; set; }
         public virtual ICollection<Review> Reviews { get; set; }
--- Models/Game.cs
+++ Models/Game.cs
@@ -16,11 +16,21 @@ namespace gamespace_api.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime ReleaseDate { get; set; }
-        public DateTime PostedDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public DateTime? PostedDate { get; set; }
+        public double TotalRating { get; set; }
+        public string ImgSrc { get; set; }
+        public int StudioId { get; set; }
+        public int GamePageId { get; set; }
         public int StatusId { get; set; }
+        public int GamePlatformId { get; set; }
+        public int GameTypeId { get; set; }
 
+        public virtual GamePage GamePage { get; set; }
+        public virtual GamePlatform GamePlatform { get; set; }
+        public virtual GameType GameType { get; set; }
         public virtual Status Status { get; set; }
+        public virtual Studio Studio { get; set; }
         public virtual ICollection<GameReview> GameReviews { get; set; }
         public virtual ICollection<GameUser> GameUsers { get; set; }
     }
